8b12dc23a8959ce4374f66d7b15baf36c15ed553
danawhite <danatroymedia@gmail.com>
2015-07-10 00:28:00 -0400

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
     output: {
         path: __dirname + '/app',
         filename: ''
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.html$/,
+                loader: 'raw'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            }
+        ]
     }
 };
